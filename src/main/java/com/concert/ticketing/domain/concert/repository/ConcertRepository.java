1fc3cbc2f86479c4169d26de10b89e4a83a357cb
NewJKH <newsun0805@gmail.com>
2025-07-08 20:20:27 +0900

    feat: 콘서트 조회 Repository 추가

--- src/main/java/com/concert/ticketing/domain/concert/repository/ConcertRepository.java
+++ src/main/java/com/concert/ticketing/domain/concert/repository/ConcertRepository.java
@@ -1,9 +1,20 @@
 package com.concert.ticketing.domain.concert.repository;
 
+import java.util.Optional;
+import java.util.function.Supplier;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.concert.ticketing.common.exception.CustomErrorCode;
+import com.concert.ticketing.common.exception.CustomException;
 import com.concert.ticketing.domain.concert.entity.Concert;
 
 public interface ConcertRepository extends JpaRepository<Concert, Long> {
 
+    Optional<Concert> getConcertById(Long id);
+
+    default Concert getConcertByIdOrThrow(Long id){
+        return this.getConcertById(id)
+                .orElseThrow((Supplier<RuntimeException>) () -> new CustomException(CustomErrorCode.CONCERT_NOT_FOUND));
+    }
 }
