e9aa6129bb59b067a8f4bf04de3ee405793a2cb8
NewJKH <newsun0805@gmail.com>
2025-07-08 20:21:27 +0900

    feat: ConcertController 조회 추거

--- src/main/java/com/concert/ticketing/domain/concert/controller/ConcertController.java
+++ src/main/java/com/concert/ticketing/domain/concert/controller/ConcertController.java
@@ -1,7 +1,14 @@
 package com.concert.ticketing.domain.concert.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.concert.ticketing.common.response.ApiResponse;
+import com.concert.ticketing.domain.concert.dto.ConcertResponse;
+import com.concert.ticketing.domain.concert.service.ConcertService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +17,12 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api")
 public class ConcertController {
 
+    private final ConcertService concertService;
+
+    @GetMapping("/concert")
+    public ResponseEntity<ApiResponse<ConcertResponse>> find() {
+    	return ResponseEntity
+    		.status(HttpStatus.OK)
+    		.body(ApiResponse.success("OK", concertService.getConcert()));
+    }
 }
