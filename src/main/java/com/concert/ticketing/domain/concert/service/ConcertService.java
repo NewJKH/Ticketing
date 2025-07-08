22552a92202b5f80a9ad5a6a6222f00b4e364fb7
NewJKH <newsun0805@gmail.com>
2025-07-08 20:21:03 +0900

    feat: ConcertService ( 조회기능 ) 추가

--- src/main/java/com/concert/ticketing/domain/concert/service/ConcertService.java
+++ src/main/java/com/concert/ticketing/domain/concert/service/ConcertService.java
@@ -2,10 +2,21 @@ package com.concert.ticketing.domain.concert.service;
 
 import org.springframework.stereotype.Service;
 
+import com.concert.ticketing.domain.concert.dto.ConcertResponse;
+import com.concert.ticketing.domain.concert.entity.Concert;
+import com.concert.ticketing.domain.concert.repository.ConcertRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ConcertService {
+
+	private final ConcertRepository concertRepository;
+
+	public ConcertResponse getConcert() {
+		Concert concert = concertRepository.getConcertByIdOrThrow(1L);
+		return ConcertResponse.of(concert);
+	}
 
 }
