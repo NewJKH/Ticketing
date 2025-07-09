0087877113a8476382ccd989c22e852a94151aad
NewJKH <newsun0805@gmail.com>
2025-07-09 11:49:27 +0900

    ref: 예외처리 Repository -> 서비스로 변경

--- src/main/java/com/concert/ticketing/domain/concert/service/ConcertService.java
+++ src/main/java/com/concert/ticketing/domain/concert/service/ConcertService.java
@@ -1,8 +1,12 @@
 package com.concert.ticketing.domain.concert.service;
 
+import java.util.Optional;
+
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import com.concert.ticketing.common.exception.CustomErrorCode;
+import com.concert.ticketing.common.exception.CustomException;
 import com.concert.ticketing.domain.concert.dto.ConcertResponse;
 import com.concert.ticketing.domain.concert.entity.Concert;
 import com.concert.ticketing.domain.concert.repository.ConcertRepository;
@@ -17,8 +21,11 @@ public class ConcertService {
 
 	@Cacheable(value = "concert", key = "1", cacheManager = "contentCacheManager")
 	public ConcertResponse getConcert() {
-		Concert concert = concertRepository.getConcertByIdOrThrow(1L);
-		return ConcertResponse.of(concert);
+		Optional<Concert> concertOpt = concertRepository.getConcertById(1L);
+		if ( concertOpt.isEmpty() ){
+			throw new CustomException(CustomErrorCode.CONCERT_NOT_FOUND);
+		}
+		return ConcertResponse.of(concertOpt.get());
 	}
 
 }
