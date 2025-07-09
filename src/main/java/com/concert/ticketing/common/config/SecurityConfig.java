e3f3650153c454811327e2f38c6d67d2492cab05
권용준 <yoyo91828h@gmail.com>
2025-07-09 17:53:10 +0900

    fix: 시큐리티 와이어프레임과 동일한 부분을 확인하고 올바르게 수정 URI 추가 완료

--- src/main/java/com/concert/ticketing/common/config/SecurityConfig.java
+++ src/main/java/com/concert/ticketing/common/config/SecurityConfig.java
@@ -1,36 +1,44 @@
 package com.concert.ticketing.common.config;
 
-
 import com.concert.ticketing.common.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)  // .csrf().disable() 방식은 더 이상 사용 안함.
+                .httpBasic(AbstractHttpConfigurer::disable) // BasicAuthenticationFilter 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // UsernamePasswordAuthenticationFilter, DefaultLoginPageGeneratingFilter 비활성화
+                .addFilterBefore(jwtFilter, SecurityContextHolderAwareRequestFilter.class)
+                // 경로별 인증 정책 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()  // 회원가입, 로그인 허용
-                        .anyRequest().authenticated()                // 그 외는 인증 필요
+                        .requestMatchers(
+                                "/api/login",
+                                "/api/signup",
+                                "/api/concert",
+                                "/api/seats"
+                        ).permitAll() // 인증 제외 경로
+                        .anyRequest().authenticated() // 나머지는 인증 필요
                 )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+                .build();
     }
 }
 
