cc76f24aadc7fbd51d69bbf04d05b4f6a8ad5bf1
easter1201 <kite1201kr@gmail.com>
2025-07-09 11:26:02 +0900

    feat: 잔여좌석 조회 controller 및 properties, gradle, ignore 구현

--- src/main/java/com/concert/ticketing/common/config/RedisConfig.java
+++ src/main/java/com/concert/ticketing/common/config/RedisConfig.java
@@ -1,2 +1,23 @@
-package com.concert.ticketing.common.config;public class RedisConfig {
+package com.concert.ticketing.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+  /* //추후 사용 위해 주석처리
+  @Bean(destroyMethod = "shutdown")
+  public RedissonClient redissonClient() {
+    Config config = new Config();
+    config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+    return Redisson.create(config);
+  }
+  */
+
+  @Bean
+  public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+    return new StringRedisTemplate(factory);
+  }
 }
