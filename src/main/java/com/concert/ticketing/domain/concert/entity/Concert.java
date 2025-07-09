5a6773d253e85a238f7176675cbf9cd0259a4c99
easter1201 <kite1201kr@gmail.com>
2025-07-09 12:17:38 +0900

    feat: 확인용 테스트 추가

--- src/main/java/com/concert/ticketing/domain/concert/entity/Concert.java
+++ src/main/java/com/concert/ticketing/domain/concert/entity/Concert.java
@@ -1,26 +1,32 @@
 package com.concert.ticketing.domain.concert.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import lombok.Setter;
 
 @Table(name = "concert")
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Concert {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    private String title;
-    private String description;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private LocalDate date;
-    private String place;
-    private String artist;
+  private String title;
+  private String description;
+
+  private LocalDate date;
+  private String place;
+  private String artist;
 }
--- src/main/java/com/concert/ticketing/domain/concert/entity/ConcertSector.java
+++ src/main/java/com/concert/ticketing/domain/concert/entity/ConcertSector.java
@@ -1,23 +1,32 @@
 package com.concert.ticketing.domain.concert.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name = "concert_sector")
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConcertSector {
-    @Id
-    @Enumerated
-    private Sector name;
 
-    private int remain;
+  @Id
+  @Enumerated
+  private Sector name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "concert_id")
-    private Concert concert;
+  private int remain;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "concert_id")
+  private Concert concert;
 }
