27a160d358d0c8b5e73b5c6d0435f33636597b30
leeuihyun <meta212121@naver.com>
2025-07-09 14:08:00 +0900

    feat : Member Error Enum 추가

--- src/main/java/com/concert/ticketing/common/exception/CustomErrorCode.java
+++ src/main/java/com/concert/ticketing/common/exception/CustomErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CustomErrorCode {
-    CONCERT_NOT_FOUND(HttpStatus.NOT_FOUND, "콘서트를 찾을 수 없습니다.");
+    CONCERT_NOT_FOUND(HttpStatus.NOT_FOUND, "콘서트를 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
