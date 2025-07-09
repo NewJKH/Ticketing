8c3171e51af33957444f899330c42f1dd2bdac8a
leeuihyun <meta212121@naver.com>
2025-07-10 01:06:41 +0900

    feat : 인증 구현에 따른 코드 수정 하드코딩되었던 Email -> Authentication 객체의 email 값 이용

--- src/main/java/com/concert/ticketing/domain/ticket/controller/TicketController.java
+++ src/main/java/com/concert/ticketing/domain/ticket/controller/TicketController.java
@@ -3,6 +3,7 @@ package com.concert.ticketing.domain.ticket.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,25 +24,25 @@ import lombok.RequiredArgsConstructor;
 public class TicketController {
 
 	private final TicketService ticketService;
-	private final String AuthenticationEmail = "test@email.com";
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<Void>> bookTicket(@RequestParam Sector sector,
-		@RequestParam Long concertId) {
+		@RequestParam Long concertId, @AuthenticationPrincipal String AuthenticationEmail) {
 
 		ticketService.bookTicket(sector, concertId, AuthenticationEmail);
 		return ResponseEntity.ok(ApiResponse.success("콘서트 티켓 예매에 성공했습니다.", null));
 	}
 
 	@GetMapping("/my")
-	public ResponseEntity<ApiResponse<List<TicketResponse>>> getMyTickets() {
+	public ResponseEntity<ApiResponse<List<TicketResponse>>> getMyTickets(
+		@AuthenticationPrincipal String AuthenticationEmail) {
 
 		List<TicketResponse> tickets = ticketService.getMyTickets(AuthenticationEmail);
 		return ResponseEntity.ok(ApiResponse.success("콘서트 티켓 조회에 성공했습니다.", tickets));
 	}
 
 	@DeleteMapping("/my")
-	public ResponseEntity<ApiResponse<Void>> deleteMyTickets() {
+	public ResponseEntity<ApiResponse<Void>> deleteMyTickets(@AuthenticationPrincipal String AuthenticationEmail) {
 
 		ticketService.deleteMyTickets(AuthenticationEmail);
 		return ResponseEntity.ok(ApiResponse.success("콘서트 티켓 삭제에 성공했습니다.", null));
