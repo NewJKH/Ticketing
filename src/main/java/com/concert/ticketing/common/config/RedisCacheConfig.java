afaa77bc74e5f8a926e26b7b0a6411f969a1450d
NewJKH <newsun0805@gmail.com>
2025-07-09 10:05:23 +0900

    feat: java8 LocalDate 직렬화/역직렬화 추가

--- src/main/java/com/concert/ticketing/common/config/RedisCacheConfig.java
+++ src/main/java/com/concert/ticketing/common/config/RedisCacheConfig.java
@@ -13,16 +13,32 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
 @Configuration
 @EnableCaching
 public class RedisCacheConfig {
 
 	@Bean
 	public CacheManager contentCacheManager(RedisConnectionFactory cf) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper
+			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+			.registerModules(new JavaTimeModule(),new Jdk8Module())
+			.registerModule(new ParameterNamesModule())
+			.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+
 		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
 			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-			.entryTtl(Duration.ofMinutes(1L));
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
+			.entryTtl(Duration.ofMinutes(10L)); // <- 1분
 
 		return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf)
 			.cacheDefaults(redisCacheConfiguration)
