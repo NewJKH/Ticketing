c99e2e3296d4116e18863d062423c54e94d26354
권용준 <yoyo91828h@gmail.com>
2025-07-10 14:47:35 +0900

    feat: 공통 에러코드 추가

--- src/main/java/com/concert/ticketing/common/exception/CustomErrorCode.java
+++ src/main/java/com/concert/ticketing/common/exception/CustomErrorCode.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 
 @Getter
 public enum CustomErrorCode {
+
+	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+	PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+	EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "등록된 이메일이 없습니다."),
 	CONCERT_NOT_FOUND(HttpStatus.NOT_FOUND, "콘서트를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
