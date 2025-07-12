db42a96350df4483a5b9d929018eadf78fd66b0a
권용준 <yoyo91828h@gmail.com>
2025-07-12 21:00:57 +0900

    feat: GlobalExceptionHandler 추가 - 예외처리하기 위해서

--- src/main/java/com/concert/ticketing/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/concert/ticketing/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.concert.ticketing.common.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +16,18 @@ public class GlobalExceptionHandler {
 		return ResponseEntity
 			.status(e.getErrorCode().getHttpStatus())
 			.body(ApiResponse.fail(e.getMessage(), new ErrorResponse(e.getErrorCode())));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ApiResponse<ErrorResponse>> exception(MethodArgumentNotValidException ex) {
+		String message = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.findFirst()
+				.map(FieldError::getDefaultMessage)
+				.orElseThrow();;
+
+		return ResponseEntity.badRequest()
+				.body(ApiResponse.fail(message, new ErrorResponse(CustomErrorCode.INVALID_INPUT_VALUE)));
 	}
 }
