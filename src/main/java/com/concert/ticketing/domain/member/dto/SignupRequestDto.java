cfa439dba7bfebdf98018cf02c47f6063e3987c2
권용준 <yoyo91828h@gmail.com>
2025-07-12 21:01:50 +0900

    feat: 로그인Dto, 회원가입Dto 어노테이션 추가

--- src/main/java/com/concert/ticketing/domain/member/dto/SignupRequestDto.java
+++ src/main/java/com/concert/ticketing/domain/member/dto/SignupRequestDto.java
@@ -1,5 +1,7 @@
 package com.concert.ticketing.domain.member.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +9,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class SignupRequestDto {
+    @NotBlank(message = "이메일을 입력하세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     String email;
+    @NotBlank(message = "비밀번호를 입력하세요.")
     String password;
+    @NotBlank(message = "본인의 이름을 입력하세요")
     String name;
 }
